6ffe118f0658b90e2a41797fcc4ff98c70b8d783
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-03-17 19:33:11 +0100

    Efects ehen mouse hover over the navigation buttons

--- src/view/components/main/MainController.java
+++ src/view/components/main/MainController.java
@@ -1,49 +1,41 @@
 package view.components.main;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import view.components.eventDescription.ActionCell;
+import javafx.scene.shape.Rectangle;
 import view.components.eventDescription.EventComponenet;
 import view.components.manageButton.ManageAction;
-import view.components.manageButton.ManageController;
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
+    private MFXButton eventsNavButton;
+    @FXML
+    private MFXButton sellingNavButton;
+    @FXML
+    private MFXButton ticketingNavButton;
+    @FXML
+    private Rectangle sellingLine;
+    @FXML
+    private Rectangle ticketingLine;
+    @FXML
+    private Rectangle eventsLine;
+
+
+    @FXML
     private VBox mainEventContainer;
     @FXML
     private StackPane mainLayout;
-    // go from table to  custom components and replace with the scroll pane
-    @FXML
-    private TableColumn description;
-    @FXML
-    private TableColumn status;
-    @FXML
-    private TableColumn tickets;
-    @FXML
-    private TableColumn revenue;
     @FXML
     private StackPane secondaryLayout;
-    @FXML
-    private HBox navigation;
-    @FXML
-    private TableView<String> eventTable;
-    @FXML
-    private TableColumn<String ,VBox> actionsColumn;
+
+
     private  static ObservableList<String[]> strings = FXCollections.observableArrayList();
     static {
         strings.add(new  String[]{"TestName 1","Lorem ipsum donor,43","24/03/2024", "Live" , "23/450", "$4500" }   );
@@ -55,15 +47,12 @@ public class MainController implements Initializable {
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-
+        NavigationHoverControl navigationHoverControl =  new NavigationHoverControl(eventsLine,sellingLine, ticketingLine,eventsNavButton,sellingNavButton,ticketingNavButton);
+        navigationHoverControl.initializeNavButtons();
         for(String [] test : strings ){
             mainEventContainer.getChildren().add(new EventComponenet(test,new ManageAction(this.secondaryLayout)));
         }
-
     }
-
-
     @FXML
     private void closeWindow(ActionEvent event) {
         this.secondaryLayout.setDisable(true);
