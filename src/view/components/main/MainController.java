99d46cc010c845d0c47fffc2c4e4e03b3efba82e
Nelson  Veríssimo <nelson.sverissimo@gmail.com>
2024-03-18 12:48:53 +0100

    merge conflit solved

--- src/view/components/main/MainController.java
+++ src/view/components/main/MainController.java
@@ -6,15 +6,21 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import view.components.eventDescription.EventComponenet;
 import view.components.events.CreateEventController;
 import view.components.manageButton.ManageAction;
 import view.utility.NavigationHoverControl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -72,5 +78,20 @@ public class MainController implements Initializable {
 
     private void displayEvents() {
         model.getEvents().forEach(e -> mainEventContainer.getChildren().add(new EventComponenet(e, new ManageAction(this.secondaryLayout))));
+    }
+
+
+    public void createTicket(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/components/ticketsGeneration/TicketsGeneration.fxml"));
+        Parent root = loader.load();
+
+        //TicketsGeneration ticketController = loader.getController();
+        //ticketController.setMainController(this);
+
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Create Ticket");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
